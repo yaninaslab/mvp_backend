fa6f1f5aa69b323604bec140267fb58aa0dbb8f9
yaninaslab <yana.belash@gmail.com>
2022-03-09 20:05:44 -0500

    Started working on API. Completed POST and PATCH requests for user

---
+++ app.py
@@ -0,0 +1,95 @@
+from email.mime import image
+import dbinteractions as dbi
+from flask import Flask, request, Response
+import json
+import hashlib
+
+import sys
+
+app = Flask(__name__)
+
+
+@app.post('/api/users')
+def signup_user():
+    try:
+        # Requesting data from the frontend
+        f_name = request.json['firstName']
+        l_name = request.json['lastName']
+        email = request.json['email']
+        password = request.json['password']
+        phone = request.json['phone']
+        # Here we are assigning variables that would be returned by a function that takes arguments in the brackets
+        new_user, login_token, user_id = dbi.signup_user(
+            f_name, l_name, email, password, phone)
+        # This is a condition saying that if the function returns a 'new_user' we're accepting it as an object and converting it to json
+        if(new_user == True):
+            new_user = {
+                "userId": user_id,
+                "firstName": f_name,
+                "lastName": l_name,
+                "email": email,
+                "phone": phone,
+                "loginToken": login_token
+            }
+            new_user_json = json.dumps(new_user, default=str)
+            # Returning response in json and request status
+            return Response(new_user_json, mimetype="application/json", status=200)
+        else:
+            return Response("Please enter valid data", mimetype="plain/text", status=400)
+# In case of error this will be returned
+    except:
+        print("Something went wrong")
+        return Response("Sorry, something is wrong with the service. Please try again later", mimetype="plain/text", status=501)
+
+
+@app.patch('/api/users')
+def update_user():
+    try:
+        # Requesting data from the frontend
+        login_token = request.json['loginToken']
+        f_name = request.json['firstName']
+        l_name = request.json['lastName']
+        email = request.json['email']
+        password = request.json['password']
+        phone = request.json['phone']
+        # Here we are assigning variables that would be returned by a function that takes arguments in the brackets
+        success, user, user_id = dbi.update_user(login_token,
+                                                 f_name, l_name, email, password, phone)
+        # In case of a success we convert the user into an object and then to json
+        if(success == True):
+            user = {
+                "userId": user_id,
+                "firstName": user[1],
+                "lastName": user[2],
+                "email": user[3],
+                "phone": user[4],
+            }
+            user_json = json.dumps(user, default=str)
+            # Returning response in json and request status
+            return Response(user_json, mimetype="application/json", status=200)
+        else:
+            return Response("Please enter valid data", mimetype="plain/text", status=400)
+# In case of error this will be returned
+    except:
+        print("Something went wrong")
+        return Response("Sorry, something is wrong with the service. Please try again later", mimetype="plain/text", status=501)
+
+
+if(len(sys.argv) > 1):
+    mode = sys.argv[1]
+else:
+    print("You must pass a mode to run this python script. Either testing or production")
+    exit()
+
+if(mode == "testing"):
+    print("Running in testing mode")
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
+elif(mode == "production"):
+    print("Running in production mode")
+    import bjoern  # type: ignore
+    bjoern.run(app, "0.0.0.0", 5006)
+else:
+    print("Please run with either testing or production. Example:")
+    print("python3.10 app.py production")
---
+++ dbinteractions.py
@@ -0,0 +1,122 @@
+
+import mariadb as db
+import dbcreds
+import secrets
+
+# Adding salt(extra characters to the password in db)
+
+
+def create_salt():
+    return secrets.token_urlsafe(10)
+
+# Creating login_token for every user session
+
+
+def create_login_token():
+    return secrets.token_urlsafe(50)
+
+# Connecting to db
+
+
+def connect_db():
+    conn = None
+    cursor = None
+    try:
+        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
+                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+    except db.OperationalError:
+        print("Something is wrong with the DB, please try again in 5 minutes")
+    except:
+        print("Something went wrong!")
+    return conn, cursor
+
+# Disconnecting from db
+
+
+def disconnect_db(conn, cursor):
+    try:
+        cursor.close()
+    except:
+        print("The issue with cursor")
+    try:
+        conn.close()
+    except:
+        print("The issue with connection")
+
+
+def signup_user(
+        f_name, l_name, email, password, phone):  # Function and its arguments
+    # We need to assign a new variable before try-except block if we're going to use it inside the block
+    new_user = None
+    conn, cursor = connect_db()
+    try:
+        # Creating salt and adding it to the column in the db
+        salt = create_salt()
+        # INSERT request with the inputs in []
+        cursor.execute(
+            "insert into user(f_name, l_name, email, password, phone, salt) values(?, ?, ?, ?, ?, ?)", [f_name, l_name, email, password, phone, salt])
+        conn.commit()
+        # The following condition checks if the insert happens
+        if(cursor.rowcount == 1):
+            # In case the query goes well, login_token is created
+            login_token = create_login_token()
+            # Assigning value to user_id with lastrowid attribute after the insert took place
+            user_id = cursor.lastrowid
+            # Another INSERT into user_session table
+            cursor.execute("insert into user_session(login_token, user_id) values(?, ?)", [
+                           login_token, user_id])
+            # Saving changes
+            conn.commit()
+            # In case sql queries are successful, new_user is set to True and returned to API
+            new_user = True
+            # In case of various errors this will be returned
+    except db.OperationalError:
+        print("Something is wrong with the DB, please try again in 5 minutes")
+    except db.ProgrammingError:
+        print("Error running DB query, please file bug report")
+    except:
+        print("Something went wrong!")
+    disconnect_db(conn, cursor)
+    # Returning variables from the function
+    return new_user, login_token, user_id
+
+
+def update_user(login_token,
+                f_name, l_name, email, password, phone):  # Function and its arguments
+    # Defining variables before try-except block
+    success = None
+    user = None
+    user_id = None
+    conn, cursor = connect_db()
+    try:
+        # SELECT request to pull down login_token from the db
+        cursor.execute(
+            "select user_id from user_session where login_token = ?", [login_token])
+        # Saving this row with fetchone()
+        user = cursor.fetchone()
+        # Assigning the correct position of user_id in the row 'user'
+        user_id = user[0]
+        # UPDATE request that's going to edit values
+        cursor.execute(
+            "update user set f_name = ?, l_name = ?, email = ?, password = ?, phone = ? where id = ?", [f_name, l_name, email, password, phone, user_id])
+        # Saving changes
+        conn.commit()
+        # The following condition checks if the query happens
+        if(cursor.rowcount == 1):
+            # SELECT query selects the necessary values and returns them as response from db
+            cursor.execute(
+                "select id, f_name, l_name, email, phone from user where id = ?", [user_id])
+            # Collecting this data with fetchone()
+            user = cursor.fetchone()
+            # If the previous line was successful, success is turned to True
+            success = True
+    except db.OperationalError:
+        print("Something is wrong with the DB, please try again in 5 minutes")
+    except db.ProgrammingError:
+        print("Error running DB query, please file bug report")
+    except:
+        print("Something went wrong!")
+    disconnect_db(conn, cursor)
+    # Returning variables from the function
+    return success, user, user_id
